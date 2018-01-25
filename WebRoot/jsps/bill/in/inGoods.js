8357378c28678bbdd4a1f9e9df6f38143e5d8a30
ziotonio <35451667+ziotonio@users.noreply.github.com>
2018-01-25 19:36:03 +0100

    Add files via upload

--- WebRoot/jsps/bill/in/inGoods.js
+++ WebRoot/jsps/bill/in/inGoods.js
@@ -3,53 +3,16 @@
  * @returns
  */
 $(function() {
-	document.writeln("<script type='text/javascript' src='lib/headSet.js'></script>");
-	document.writeln("<script type='text/javascript' src='lib/inGoodsFanOut.js'></script>");
+	document.writeln("<script type='text/javascript' src='lib/setterFun.js'></script>");	
+	document.writeln("<script type='text/javascript' src='lib/queryMsgFun.js'></script>");	
 
 
-		$("#query").click(function() {
-			$("[name='pageNum']").val(1);
-			$("form:first").submit();
-		});
-		$(".ajaxMsg").live("click",function(){
-			$(".ajaxMsg").empty();
-		});
-		$(".info").click(function(){
-			var jsonParam = {"bqm.goodsUuid":$(this).attr("value")};
-			jsonParam["bqm.time"]= $("[name='bqm.time']").val();
-			jsonParam["bqm.time2"]= $("[name='bqm.time2']").val();
-			jsonParam["bqm.type"]= $("[name='bqm.type']").val();
-			$tt = $(this).parent().parent();
-				//将制定标记tr删除
-				$('.ajaxMsg').empty();
-				
-				//每个tr对象都带有一个class="ajaxMsg",用于后期操作删除标记
-				
-				//动态添加一个tr行,用于做标题栏
-				//创建tr组件
-				
-				var tCodes = setHead(tt);
-				trHead = tCodes[0];
-				tt = tCodes[1];
-				//--------------------------------------------------------------------------
-				tCodes = setTr(tr, tt);
-				tr = tCodes[0];
-				tt = tCodes[1];
-				
-				//--------------------------------------------------------------------------
-				
-				
-				var $trFoot = $("<tr align='center' class='ajaxMsg'></tr>");
-				var $td1 = $("<td align='right' colspan='4' height='30'>总计：</td>");
-				$trFoot.append($td1);
-				var $td2 = $("<td align='right'>"+intToFloat(sum)+"&nbsp;元</td>");
-				$trFoot.append($td2);
-				$tt.after($trFoot);
-				$tt=$trHead;
-				
-		});
-		function intToFloat(val){
-			return new Number(val).toFixed(2);
-		}
-		
+
+	$("#query").click(queryFun());
+	$(".ajaxMsg").live("click",msgFun());
+	$(".info").click(setFun());
+	function intToFloat(val){
+		return new Number(val).toFixed(2);
+	}
+
 });
