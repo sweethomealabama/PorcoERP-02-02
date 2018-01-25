5d305349664919bd850f973407e3d0dfb7380403
ziotonio <35451667+ziotonio@users.noreply.github.com>
2018-01-25 19:11:28 +0100

    Add files via upload

--- WebRoot/WEB-INF/jsps/bill/buyBill.js
+++ WebRoot/WEB-INF/jsps/bill/buyBill.js
@@ -4,29 +4,12 @@
  */
 $(function() {
 	document.writeln("<script type='text/javascript' src='buyBillFanOut.js'></script>");
+	document.writeln("<script type='text/javascript' src='formFirst.js'></script>");
 
-		$("#query").click(function() {
-			$("form:first").submit();
-		});
+
+		$("#query").click(formFirst());
 		
-		$(".info").click(function(){
-			$nowTr = $(this).parent().parent();
-			
-			$(".ajaxMsg").remove();
-			
-			//将当前货物的uuid配置公共查询条件，发送ajax请求，获取对应货物的所有明细数据
-			var jsonParma = {};
-			jsonParma["bqm.goodsUuid"] = $(this).attr("val");
-			jsonParma["bqm.begin"] = $("[name='bqm.begin']").val();
-			jsonParma["bqm.end"] = $("[name='bqm.end']").val();
-			jsonParma["bqm.type"] = $("[name='bqm.type']").val();
-			jsonParma["bqm.supplierUuid"] = $("[name='bqm.supplierUuid']").val();
-			$.post("bill_ajaxBillDetailByGoods.action",jsonParma, dataBill(data));
-			
-			$(".ajaxMsg").live("click",function(){
-				$(".ajaxMsg").remove();
-			});
-		});
+		$(".info").click(messageFun());
 		
 		
 	});
---
+++ WebRoot/WEB-INF/jsps/bill/formFirst.js
@@ -0,0 +1,28 @@
+/**
+ * called in buyBill.js
+ * @returns
+ */
+function formFirst() {
+			$("form:first").submit();
+}
+
+function removeFun(){
+	$(".ajaxMsg").remove();
+}
+
+function messageFun(){
+	$nowTr = $(this).parent().parent();
+	
+	$(".ajaxMsg").remove();
+	
+	//将当前货物的uuid配置公共查询条件，发送ajax请求，获取对应货物的所有明细数据
+	var jsonParma = {};
+	jsonParma["bqm.goodsUuid"] = $(this).attr("val");
+	jsonParma["bqm.begin"] = $("[name='bqm.begin']").val();
+	jsonParma["bqm.end"] = $("[name='bqm.end']").val();
+	jsonParma["bqm.type"] = $("[name='bqm.type']").val();
+	jsonParma["bqm.supplierUuid"] = $("[name='bqm.supplierUuid']").val();
+	$.post("bill_ajaxBillDetailByGoods.action",jsonParma, dataBill(data));
+	
+	$(".ajaxMsg").live("click",removeFun());
+}
