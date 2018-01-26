8715405c7545e23f3be6689de26a12b0ea08c77a
fedmask <33149307+fedmask@users.noreply.github.com>
2018-01-26 17:44:37 +0100

    Add files via upload

--- WebRoot/WEB-INF/jsps/bill/formFirstOut.js
+++ WebRoot/WEB-INF/jsps/bill/formFirstOut.js
@@ -10,12 +10,7 @@ function messageFun(){
 	$(".ajaxMsg").remove();
 	
 	//将当前货物的uuid配置公共查询条件，发送ajax请求，获取对应货物的所有明细数据
-	var jsonParma = {};
-	jsonParma["bqm.goodsUuid"] = $(this).attr("val");
-	jsonParma["bqm.begin"] = $("[name='bqm.begin']").val();
-	jsonParma["bqm.end"] = $("[name='bqm.end']").val();
-	jsonParma["bqm.type"] = $("[name='bqm.type']").val();
-	jsonParma["bqm.supplierUuid"] = $("[name='bqm.supplierUuid']").val();
+	var jsonParma = jsonFill();
 	$.post("bill_ajaxBillDetailByGoods.action",jsonParma, dataBill(data));
 	
 	$(".ajaxMsg").live("click",removeFun());
