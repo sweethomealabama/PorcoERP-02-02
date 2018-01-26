0b201a4493203baf5912759b2f406bc4cf8987c3
fedmask <33149307+fedmask@users.noreply.github.com>
2018-01-26 17:28:39 +0100

    Add files via upload

--- WebRoot/WEB-INF/jsps/store/in/lib/clickEvent.js
+++ WebRoot/WEB-INF/jsps/store/in/lib/clickEvent.js
@@ -3,7 +3,8 @@
  * @returns
  */
 function clickEvent(){
-	document.writeln("<script type='text/javascript' src='pickerFun.js'></script>");
+	document.writeln("<script type='text/javascript' src='assignJson.js'></script>");
+
 
 			//真实的入库操作
 			//传递到后台：哪个商品？入到哪个仓库？入多少个？
@@ -12,7 +13,5 @@ function clickEvent(){
 			var jsonParam = {};
 			jsonParam["goodsUuid"] = $nowTr.prev().attr("gm");
 			jsonParam["storeUuid"] = $nowTr.children("td:eq(1)").children("select").val();
-			jsonParam["num"] = $nowTr.children("td:eq(3)").children("input").val();
-			jsonParam["odmUuid"] = $nowTr.prev().attr("odm");
-			$.post("store_ajaxInGoods.action",jsonParam,pickerFun(data));
+			assignJson(jsonParam);
 }
