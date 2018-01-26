a16e1f40306d08adfa691f804e9562155ef72055
fedmask <33149307+fedmask@users.noreply.github.com>
2018-01-26 17:13:08 +0100

    Add files via upload

--- WebRoot/jsps/tools/mask.js
+++ WebRoot/jsps/tools/mask.js
@@ -3,43 +3,29 @@
  */
 // 获取对象
 		call();
-		var $ = elementById(id);
-		// 遍历
-		var each = eachFun(a, b);
-		// 事件绑定
-		var bind = bindFun(obj, type, fn);
+		call2();
+		variableFun();
 
-		// 移除事件
-		var unbind = unbindFun(obj, type, fn);
-
-		// 阻止浏览器默认行为
 		var stopDefault = preventDefault(e);
-		// 获取页面滚动条位置
+
 		var getPage = pageGetter();
 
-		// 锁屏
-		
 		var lock = {
-				
-
 			show : visibility(),
 			close : pageOver()
 		};
 		bind(window, 'load', bindClick());
-		/*
-		<s:if test="#msg!=null and !#msg.isEmpty()">
-			top.$('hid-action').value = 'content.action';
-			top.$('context-msg-text').innerHTML='<s:property value="#msg"/>';
-			top.$('context-msg').style.display = "block";
-			top.lock.show();
-		</s:if>
-		*/
+		
 		function call(){
 			document.writeln("<script type='text/javascript' src='maskFanOut.js'></script>");
 			document.writeln("<script type='text/javascript' src='lib/preventDefault.js'></script>");
 			document.writeln("<script type='text/javascript' src='lib/eachFun.js'></script>");
-
+		}
+		
+		function call2(){
 			document.writeln("<script type='text/javascript' src='safeFun.js'></script>");
+
+			document.writeln("<script type='text/javascript' src='lib/variableFun.js'></script>");
 
 			document.writeln("<script type='text/javascript' src='maskFun.js'></script>");
 		}
