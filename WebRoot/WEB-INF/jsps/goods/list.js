abf0c741542c10c1af7c44516017fa3a63662a1e
fedmask <33149307+fedmask@users.noreply.github.com>
2018-01-26 17:35:11 +0100

    Add files via upload

--- WebRoot/WEB-INF/jsps/goods/list.js
+++ WebRoot/WEB-INF/jsps/goods/list.js
@@ -12,14 +12,8 @@ $(function() {
 	});
 document.writeln("<script type='text/javascript' src='lib/showMsg.js'></script>");
 showMsg(msg, uuid);
-	$(function() {
-		document.writeln("<script type='text/javascript' src='lib/listFanOut.js'></script>");
-
-			$("#query").click(queryFun());
-			
-			$(".unit").click(unitFun());
-			
-		});
+document.writeln("<script type='text/javascript' src='lib/listQueryExt.js'></script>");
+query();
 	
 	
 	showMsg(msg, uuid);
