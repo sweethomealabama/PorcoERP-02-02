4b1751aa26684e83ecb02b551cdae94bd5903fa3
sweethomealabama <35634228+sweethomealabama@users.noreply.github.com>
2018-01-31 15:30:30 +0100

    Add files via upload

--- WebRoot/jsps/tools/mask.js
+++ WebRoot/jsps/tools/mask.js
@@ -2,30 +2,22 @@
  * this file is called in mask.jsp file
  */
 // 获取对象
-		call();
-		call2();
+		document.writeln("<script type='text/javascript' src='maskFanOut.js'></script>");
+		document.writeln("<script type='text/javascript' src='lib/preventDefault.js'></script>");
+		document.writeln("<script type='text/javascript' src='lib/eachFun.js'></script>");
+		document.writeln("<script type='text/javascript' src='safeFun.js'></script>");
+		document.writeln("<script type='text/javascript' src='lib/variableFun.js'></script>");
+		document.writeln("<script type='text/javascript' src='maskFun.js'></script>");
+		
 		variableFun();
 
 		var stopDefault = preventDefault(e);
 
 		var getPage = pageGetter();
-
-		var lock = {
-			show : visibility(),
-			close : pageOver()
-		};
+		
+		document.writeln("<script type='text/javascript' src='lib/lockFun.js'></script>");
+		lockFun();
+		
 		bind(window, 'load', bindClick());
 		
-		function call(){
-			document.writeln("<script type='text/javascript' src='maskFanOut.js'></script>");
-			document.writeln("<script type='text/javascript' src='lib/preventDefault.js'></script>");
-			document.writeln("<script type='text/javascript' src='lib/eachFun.js'></script>");
-		}
 		
-		function call2(){
-			document.writeln("<script type='text/javascript' src='safeFun.js'></script>");
-
-			document.writeln("<script type='text/javascript' src='lib/variableFun.js'></script>");
-
-			document.writeln("<script type='text/javascript' src='maskFun.js'></script>");
-		}
