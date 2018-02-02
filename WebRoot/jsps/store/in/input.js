dea8776e69be01bf98057a2ef87689ec078b9c57
sweethomealabama <35634228+sweethomealabama@users.noreply.github.com>
2018-02-02 11:53:29 +0100

    Add files via upload

--- WebRoot/jsps/store/in/input.js
+++ WebRoot/jsps/store/in/input.js
@@ -18,43 +18,6 @@ $(function() {
 
 		
 		$(".oper").click(onClick(storeUuiArr, storeNameArr));
-		$(".ajaxIn").live("click",function(){
-			//0.页面校验输入是否合法（省略）
-			//1.组织ajax提交的数据
-			jsonParam ={};
-			//主单编号
-			jsonParam["odm.order.uuid"] = omUuid;
-			//获取当前链接所在行的上一行中隐藏的第一个子节点和第二个子节点的值
-			//子单编号
-			jsonParam["odm.uuid"] = $(this).parent().parent().prev().children("input:eq(0)").val();
-			//货物编号
-			jsonParam["odm.goods.uuid"] = $(this).parent().parent().prev().children("input:eq(1)").val();
-			//入库数量
-			jsonParam["som.num"] = $(this).parent().prev().children("input:eq(0)").val();
-			//仓库编号
-			jsonParam["som.sdm.sm.uuid"] = $(this).parent().parent().children("td:eq(1)").children("select").val();
-			
-			//为ajax提交操作后的操作对象进行初始化
-			var $upTr = $(this).parent().parent().prev();
-			var $upCenter =  $upTr.children("td:eq(2)");
-			var $upRight = $upTr.children("td:eq(3)");
-			var $myTr = $(this).parent().parent();
-				if(false){
-					//显示返回按钮
-					$("#inOrderTitle").remove();
-					$("#inOrder").remove();
-					$("#allInTitle").css("display","block");
-					$("#return").css("display","block");
-					return;
-				}
-				
-				if(0 == 0){
-					$upTr.remove();
-					$myTr.remove();
-				}else{
-					$upCenter.text(20);
-					$upRight.text(10);	
-				}
-			});
+		$(".ajaxIn").live("click", ajOnClick());
 			
 	});
